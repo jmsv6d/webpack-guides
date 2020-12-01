fa40b7f1af18bbebf46fd0d4b55d6d71d70b78a3
Jaime Mackey <jaime.mackey@cerner.com>
2020-12-01 13:03:23 -0600

    Optimization

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css';
+import { cube } from './math.js';
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
+  function component() {
+   const element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   // Lodash, now imported by this script
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+   element.innerHTML = [
+     'Hello webpack!',
+     '5 cubed is equal to ' + cube(5)
+   ].join('\n\n');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
+    return element;
+  }
 
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
-
-if (module.hot) {
- module.hot.accept('./print.js', function() {
-   console.log('Accepting the updated printMe module!');
-   printMe();
- })
-}
+  document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-     app: './src/index.js',
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Hot Module Replacement',
-    }),
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+ mode: 'development',
+ optimization: {
+   usedExports: true,
+ },
 };
